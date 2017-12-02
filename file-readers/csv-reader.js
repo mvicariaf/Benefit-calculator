8a5781c77d1f8456bfc5d3066541e25bd0a01e2d
mvicariaf <amivicaria@hotmail.es>
2017-12-02 13:39:19 +0100

    Refactor csv reader to a more idiomatic javascript ES2015.

--- file-readers/csv-reader.js
+++ file-readers/csv-reader.js
@@ -1,21 +1,30 @@
 const fs = require('fs');
 
+const CELL_SEPARATOR = ";";
+
+const removeLineBreaks = (string) => {
+ return string.replace(/(\r\n|\n|\r)/gm,"");
+};
+
+const readCsvHeaders = (rows) => {
+    const headersRow = rows.shift();
+    return removeLineBreaks(headersRow).split(CELL_SEPARATOR);
+};
+
+const csvDataRowToJson = (headers, row) => {
+    const cells = removeLineBreaks(row).split(CELL_SEPARATOR);
+    return headers.reduce((json, header, index) => {
+        json[header] = cells[index];
+        return json;
+    }, {});
+};
+
 const csvReader = (file) => {
     let csvFile = fs.readFileSync(file, {encoding: 'utf-8'});
     let rows = csvFile.split("\n");
 
-    const headers = rows.shift().replace(/(\r)/gm,"").split(";");
-
-    let json = [];
-    rows.forEach(function(row){
-        const jsonRow = {};
-        const cells = row.replace(/(\r)/gm,"").split(";");
-        for(let i = 0; i < headers.length; i++){
-            jsonRow[headers[i]] = cells[i];
-        }
-        json.push(jsonRow);
-    });
-    return json;
+    const headers = readCsvHeaders(rows);
+    return rows.map((dataRow) => csvDataRowToJson(headers, dataRow));
 };
 
 module.exports = csvReader;
